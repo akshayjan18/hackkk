06960b54ac0ccbc2025760926f1ef087e911202c
AkshayVvv <65759558+AkshayVvv@users.noreply.github.com>
2020-10-20 18:30:51 +0530

    Update enterinteger.c

--- enterinteger.c
+++ enterinteger.c
@@ -1,10 +1,18 @@
-//
-int main()
-{
-    int number;
-    printf("Enter an integer:");
-     scanf("%d",&number)
-    printf("You entered: %d", number);
-    return 0;
+#include<stdio.h>  
+int main()    
+    {    
+    int n,r,sum=0,temp;    
+    printf("enter the number=");    
+    scanf("%d",&n);    
+    temp=n;    
+    while(n>0)    
+    {    
+    r=n%10;    
+    sum=(sum*10)+r;    
+    n=n/10;    
+    }    
+    if(temp==sum)    
+    printf("palindrome number ");    
+    else    
+    printf("not palindrome");   
 }
-
